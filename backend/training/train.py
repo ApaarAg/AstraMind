6f9152c3f67352cdeeef2baa3283400d90e4edd2
Apaar Agarwal <apaaragarwal117@gmail.com>
2026-02-27 19:37:50 +0530

    feat(model): implement diminishing returns gain function and improve target realism

--- backend/training/train.py
+++ backend/training/train.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score,mean_squared_error
 from xgboost import XGBRegressor
-import joblib ,os
+import joblib ,os,json
 
 np.random.seed(67)
 
@@ -18,7 +18,23 @@ data=pd.DataFrame({
     "confidence":np.random.randint(1,6,n_samples)
 })
 
-data["mastery"]=(
+# data["mastery"]=(
+#     0.4*(data["past_score"]/100)+
+#     0.25*(data["hours_spent"]/20)+
+#     0.2*(data["revision_count"]/10)+
+#     0.1*(data["confidence"]/5)-
+#     0.2*(data["difficulty"]/5)-
+#     0.15*(1/(data["days_to_exam"]+1))
+# )
+FEATURES = [
+    "difficulty",
+    "past_score",
+    "hours_spent",
+    "revision_count",
+    "days_to_exam",
+    "confidence"
+]
+baseline=(
     0.4*(data["past_score"]/100)+
     0.25*(data["hours_spent"]/20)+
     0.2*(data["revision_count"]/10)+
@@ -26,13 +42,36 @@ data["mastery"]=(
     0.2*(data["difficulty"]/5)-
     0.15*(1/(data["days_to_exam"]+1))
 )
+baseline+= np.random.normal(0,0.05,n_samples)
+baseline=np.clip(baseline,0,1)
+# improvement_factor=(
+#     0.25*(1-baseline)*
+#     (1/data["difficulty"])*
+#     (1/(data["days_to_exam"]+1))
+# )
+# gain=improvement_factor+np.random.normal(0,0.02,n_samples)
+# gain=np.clip(gain,0,0.2)
+diminishing = np.exp(-data["hours_spent"]/10)
+gain=(
+    0.40*(1-baseline)*
+    (1/data["difficulty"])*
+    (1/(data["days_to_exam"]+1))*
+    diminishing
+)
 
-data["mastery"]+=np.random.normal(0,0.005,n_samples)
+gain+=np.random.normal(0,0.01,n_samples)
+gain=np.clip(gain,0,0.3)
 
-data["mastery"]=np.clip(data["mastery"],0,1)
 
-X=data.drop("mastery",axis=1)
-y=data["mastery"]
+data["gain"]=gain
+
+# data["mastery"]+=np.random.normal(0,0.005,n_samples)
+
+# data["mastery"]=np.clip(data["mastery"],0,1)
+
+
+X=data[FEATURES]
+y=data["gain"]
 
 X_train,X_test,Y_train,Y_test=train_test_split(X,y,test_size=0.2,random_state=89)
 
@@ -41,8 +80,21 @@ model=XGBRegressor(
     max_depth=4,
     learning_rate=0.05
 )
-
+print("Gain mean:", gain.mean())
+print("Gain variance:", gain.var())
 model.fit(X_train,Y_train)
+
+Base_dir=os.path.dirname(os.path.dirname(__file__))
+DATA_dir=os.path.join(Base_dir,"data")
+os.makedirs(DATA_dir,exist_ok=True)
+importances=model.feature_importances_
+importances_dict={
+    feature:float(importance)
+    for feature, importance in zip(FEATURES,importances)
+}
+
+with open(os.path.join(DATA_dir,"feature_importance.json"),"w") as f:
+    json.dump(importances_dict,f,indent=4)
 
 preds=model.predict(X_test)
 
